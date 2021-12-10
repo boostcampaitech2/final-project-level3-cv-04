a3a7135e6578cb903a2840f1fbf4edccfb682199
YoonyoungL <yyunglee97@gmail.com>
2021-12-10 10:23:35 +0000

    feat: Add Focal Loss

--- model_lab/video_classification/model/loss.py
+++ model_lab/video_classification/model/loss.py
@@ -1,9 +1,30 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+# https://discuss.pytorch.org/t/is-this-a-correct-implementation-for-focal-loss-in-pytorch/43327/8
+class FocalLoss(nn.Module):
+    def __init__(self, weight=None,
+                 gamma=2., reduction='mean'):
+        nn.Module.__init__(self)
+        self.weight = weight
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, input_tensor, target_tensor):
+        log_prob = F.log_softmax(input_tensor, dim=-1)
+        prob = torch.exp(log_prob)
+        return F.nll_loss(
+            ((1 - prob) ** self.gamma) * log_prob,
+            target_tensor,
+            weight=self.weight,
+            reduction=self.reduction
+        )
 
 _criterion_entrypoints = {
-    'cross_entropy': nn.CrossEntropyLoss
+    'cross_entropy': nn.CrossEntropyLoss,
+    'focal_loss' : FocalLoss
 }
-
 
 def criterion_entrypoint(criterion_name):
     return _criterion_entrypoints[criterion_name]
