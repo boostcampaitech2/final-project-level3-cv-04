7128905d135d1f3da8c50e932d640e20d72280c3
castleUk <jo631@naver.com>
2021-12-22 07:50:01 +0000

    chore: Update config

--- model_lab/frame_classification/custom/full/recipe.py
+++ model_lab/frame_classification/custom/full/recipe.py
@@ -8,13 +8,14 @@ class Recipe(AbstractRecipe):
 		from albumentations.pytorch import ToTensorV2
 
 		trainTransform = A.Compose([
-			A.Resize(224,224),
+			A.Resize(512,512),
 			A.OneOf([
 				A.Flip(p=1.0),
-				# A.RandomRotate90(p=1.0),
-				# A.ShiftScaleRotate(p=1.0),
-			], p=0.5),
-			A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.15, p=0.5),
+				A.RandomRotate90(p=1.0),
+				A.ShiftScaleRotate(p=1.0),
+			], p=0.6),
+			A.RandomBrightnessContrast(),
+			A.HueSaturationValue(),
 			A.GaussNoise(p=0.3),
 			A.OneOf([
 					A.Blur(p=1.0),
@@ -22,28 +23,29 @@ class Recipe(AbstractRecipe):
 					A.MedianBlur(blur_limit=5, p=1.0),
 					A.MotionBlur(p=1.0),
 			], p=0.1),
+			A.CLAHE(),
 			A.Normalize(), 
 			ToTensorV2()])
 
 		validTransform = A.Compose([
-			A.Resize(224,224),
+			A.Resize(512,512),
 			A.Normalize(),
 			ToTensorV2()])
 
 		return trainTransform, validTransform
 
-	# def make_weights_for_balanced_classes(self, dataset, nclasses):                        
-	# 	count = [0] * nclasses                                                      
-	# 	for i in range(len(dataset)):                                                         
-	# 			count[dataset.getLabelForWeighted(i)] += 1  
-	# 	weight_per_class = [0.] * nclasses                                      
-	# 	avg = float(sum(count)) / len(count)                    
-	# 	for i in range(nclasses):                                                   
-	# 			weight_per_class[i] = avg/float(count[i])     
-	# 	weight = [0] * len(dataset)   
-	# 	for i in range(len(dataset)):                                          
-	# 			weight[i] = weight_per_class[dataset.getLabelForWeighted(i)]                                  
-	# 	return weight
+	def make_weights_for_balanced_classes(self, dataset, nclasses):                        
+		count = [0] * nclasses                                                      
+		for i in range(len(dataset)):                                                         
+				count[dataset.getLabelForWeighted(i)] += 1  
+		weight_per_class = [0.] * nclasses                                      
+		avg = float(sum(count)) / len(count)                    
+		for i in range(nclasses):                                                   
+				weight_per_class[i] = avg/float(count[i])     
+		weight = [0] * len(dataset)   
+		for i in range(len(dataset)):                                          
+				weight[i] = weight_per_class[dataset.getLabelForWeighted(i)]                                  
+		return weight
 
 	def _buildDataloader(self):
 		from torch.utils.data import DataLoader
@@ -53,16 +55,16 @@ class Recipe(AbstractRecipe):
 
 		tDataset = WashingDataset("train",self.config["root"],self.config["train_csv"],trainT)
 
-		# w = self.make_weights_for_balanced_classes(tDataset,self.config["num_classes"])
-		# sampler = WeightedRandomSampler(w, len(w), replacement=True) 
+		w = self.make_weights_for_balanced_classes(tDataset,self.config["num_classes"])
+		sampler = WeightedRandomSampler(w, len(w), replacement=True) 
 		self.trainDataloader = DataLoader(
 			dataset = tDataset,
 			batch_size= self.config["batch"],
-			shuffle=True,
+			# shuffle=True,
 			drop_last=True,
 			num_workers=4,
-			pin_memory=True
-			# sampler=sampler
+			pin_memory=True,
+			sampler=sampler
 		)
 		self.validDataloader = DataLoader(
 			dataset = WashingDataset("valid",self.config["root"],self.config["valid_csv"],validT),
@@ -79,7 +81,7 @@ class Recipe(AbstractRecipe):
 		import torch.nn
 		
 		self.model = timm.create_model(
-			model_name="mobilenetv3_rw",
+			model_name="mobilenetv3_large_100",
 			pretrained=pretrained,
 			num_classes=self.config["num_classes"],
 			act_layer=torch.nn.ReLU
--- model_lab/frame_classification/custom/golden/recipe.py
+++ model_lab/frame_classification/custom/golden/recipe.py
@@ -13,8 +13,9 @@ class Recipe(AbstractRecipe):
 				A.Flip(p=1.0),
 				A.RandomRotate90(p=1.0),
 				A.ShiftScaleRotate(p=1.0),
-			], p=0.75),
-			A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.15, p=0.5),
+			], p=0.6),
+			A.RandomBrightnessContrast(),
+			A.HueSaturationValue(),
 			A.GaussNoise(p=0.3),
 			A.OneOf([
 					A.Blur(p=1.0),
@@ -22,6 +23,8 @@ class Recipe(AbstractRecipe):
 					A.MedianBlur(blur_limit=5, p=1.0),
 					A.MotionBlur(p=1.0),
 			], p=0.1),
+			A.CLAHE(),
+			A.ElasticTransform(),
 			A.Normalize(), 
 			ToTensorV2()])
 
@@ -39,8 +42,8 @@ class Recipe(AbstractRecipe):
 		weight_per_class = [0.] * nclasses                                      
 		avg = float(sum(count)) / len(count)                    
 		for i in range(nclasses):                                                   
-				weight_per_class[i] = avg/float(count[i])  
-		print(weight_per_class)         
+				weight_per_class[i] = avg/float(count[i])     
+		print(weight_per_class)
 		weight = [0] * len(dataset)   
 		for i in range(len(dataset)):                                          
 				weight[i] = weight_per_class[dataset.getLabelForWeighted(i)]                                  
@@ -48,28 +51,30 @@ class Recipe(AbstractRecipe):
 
 	def _buildDataloader(self):
 		from torch.utils.data import DataLoader
-		from src.dataset import WashingDataset
+		from src.kaggle_dataset import WashingDataset
 		from torch.utils.data.sampler import WeightedRandomSampler
 		trainT, validT = self._getTransform()
 
 		tDataset = WashingDataset("train",self.config["root"],self.config["train_csv"],trainT)
 
-		w = self.make_weights_for_balanced_classes(tDataset,self.config["num_classes"])
-		sampler = WeightedRandomSampler(w, len(w), replacement=True) 
+		# w = self.make_weights_for_balanced_classes(tDataset,self.config["num_classes"])
+		# sampler = WeightedRandomSampler(w, len(w), replacement=True) 
 		self.trainDataloader = DataLoader(
 			dataset = tDataset,
 			batch_size= self.config["batch"],
-			# shuffle=True,
+			shuffle=True,
 			drop_last=True,
 			num_workers=4,
-			sampler=sampler
+			pin_memory=True,
+			# sampler=sampler
 		)
 		self.validDataloader = DataLoader(
 			dataset = WashingDataset("valid",self.config["root"],self.config["valid_csv"],validT),
 			batch_size= self.config["batch"],
 			shuffle=False,
 			drop_last=False,
-			num_workers=2
+			num_workers=2,
+			pin_memory=True
 		)
 
 	
@@ -78,17 +83,18 @@ class Recipe(AbstractRecipe):
 		import torch.nn
 		
 		self.model = timm.create_model(
-			model_name="mobilenetv3_rw",
+			model_name="efficientnet_b5",
 			pretrained=pretrained,
 			num_classes=self.config["num_classes"],
-			act_layer=torch.nn.ReLU
+			# act_layer=torch.nn.ReLU
 		)
 
 	def _buildOptimizer(self):
 		self.optimizer = torch.optim.AdamW(params=self.model.parameters(),lr=0)
 
 	def _buildLoss(self):
-		self.loss = torch.nn.CrossEntropyLoss()
+		# self.loss = torch.nn.CrossEntropyLoss()
+		self.loss = FocalLoss()
 
 	def _buildScheduler(self):
 		self.scheduler = CosineAnnealingWarmUpRestarts(self.optimizer, eta_max= self.config["lr"],T_0=len(self.trainDataloader)*10 , T_up=len(self.trainDataloader),gamma=0.9)
@@ -101,6 +107,27 @@ class Recipe(AbstractRecipe):
 		self._buildScheduler()
 
 
+import torch.nn as nn
+class FocalLoss(nn.Module):
+	def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+		super(FocalLoss, self).__init__()
+		self.alpha = alpha
+		self.gamma = gamma
+		self.logits = logits
+		self.reduce = reduce
+		self.ce = nn.CrossEntropyLoss(reduction="none")
+
+	def forward(self, inputs, targets):
+	
+		ce_loss = self.ce(inputs, targets)
+
+		pt = torch.exp(-ce_loss)
+		F_loss = self.alpha * (1-pt)**self.gamma * ce_loss
+
+		if self.reduce:
+				return torch.mean(F_loss)
+		else:
+				return F_loss
 
 import math
 from torch.optim.lr_scheduler import _LRScheduler
